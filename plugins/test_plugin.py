e93160fcbb8a7a69c35d73194fb3a5a1a0118491
jakub <jakub@archeus>
2023-04-11 18:46:18 +0200

    git python-server init

---
+++ plugins/test_plugin.py
@@ -0,0 +1,10 @@
+#!/bin/env python3
+
+
+def to_be_imported(arg1,arg2,arg3):
+    #this is a help message
+    return [*args]
+
+def _no_to_be_imported():
+    #you shoudn't see this
+    return "err"
---
+++ server.py
@@ -0,0 +1,27 @@
+#!/bin/env python3
+
+from importlib import reload
+import os
+import sys
+import threading
+import socket
+import websocket
+import json
+import pickle
+import numpy as np
+
+import plugins/*
+
+def _help():
+	print("help is missing")
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	print("starting server")
+	_help()
