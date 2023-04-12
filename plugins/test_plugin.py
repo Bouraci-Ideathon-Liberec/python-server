9cb1da13578cd782b8733f3ce95cde8472c3aa76
jakub <jakub@archeus>
2023-04-12 12:22:20 +0200

    server is halfway running

--- plugins/test_plugin.py
+++ plugins/test_plugin.py
@@ -11,21 +11,6 @@ def to_be_imported(arg1,arg2,arg3):
     '''
     return [*args]
 
-def web_add(int1=0,int2=1):
-	'''
-    this is documntary comment
-    :param int1: intiger
-    :param int2: intiger
-
-    :return: intiger
-    '''
-
-
-	return int1+int2
-
-def web_mult(int1=0,int2=1):
-	return int1*int2
-
 def _no_to_be_imported():
     #you shoudn't see this
     return "err"
